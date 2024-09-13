f25c8bef46776b28d287e8223869a33a2426a7c1
Vinicius <Costa>
2024-09-12 22:31:58 -0300

    refactor: update button component styling

--- src/components/atom/button/button.tsx
+++ src/components/atom/button/button.tsx
@@ -3,7 +3,9 @@ import { ComponentProps, forwardRef } from 'react'
 
 const Button = forwardRef<HTMLButtonElement, ComponentProps<'button'>>(
   ({ className, ...props }, ref) => {
-    return <button ref={ref} className={cn(className, '')} {...props} />
+    return (
+      <button ref={ref} className={cn(className, 'bg-slate-50')} {...props} />
+    )
   },
 )
 
